a1d9e9f37f82774c84146404eb07aa22b1cab8ec
20017757 <20017757@LAPTOP-545AO3G7>
2021-07-29 14:56:46 +0800

    Currency class added

---
+++ C206_CaseStudy/src/Currency.java
@@ -0,0 +1,27 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Wong Thai An, 29 Jul 2021 2:48:17 pm
+ */
+
+/**
+ * @author 20017757
+ *
+ */
+public class Currency {
+	private String ISO;
+	private String name;
+	private double bRate;
+	private double sRate;
+	
+	public Currency(String ISO, String name, double bRate, double sRate) {
+		this.ISO = ISO;
+		this.name = name;
+		this.bRate = bRate;
+		this.sRate = sRate;
+	}
+	
+	
+}
