00dbfdb36e104b7e5b5c94d7db3a4ef93d1cac63
20017757 <20017757@LAPTOP-545AO3G7>
2021-07-29 15:32:10 +0800

    setHeader added

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,21 @@
-
+import java.util.ArrayList;
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
-		System.out.println("Hello World!");
+		ArrayList<Currency> currencyList = new ArrayList<Currency>();
+		
+		
+		int option = 0;
+		
 	}
-
+	
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
+	public static void menu() {
+		C206_CaseStudy.setHeader("MONEY EXCHANGE MANAGEMENT SYSTEM");
+	}
 }
