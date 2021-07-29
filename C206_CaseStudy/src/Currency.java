1f48f8b39f5ac630f8e633e30ce10dda79c204f4
WongThaiAn <87117827+WongThaiAn@users.noreply.github.com>
2021-07-29 18:22:50 +0800

    Update Currency.java

--- C206_CaseStudy/src/Currency.java
+++ C206_CaseStudy/src/Currency.java
@@ -1,15 +1,4 @@
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * Wong Thai An, 29 Jul 2021 2:48:17 pm
- */
 
-/**
- * @author 20017757
- *
- */
 public class Currency {
 	private String ISO;
 	private String name;
