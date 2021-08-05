03ca34ec34b4994d48ef103ea2a17f2cf8a01d5e
20043629 <20043629@LAPTOP-3BTDFME2.myrepublic.com.sg>
2021-08-05 11:17:13 +0800

    updated viewAllCurrency()

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -89,7 +89,7 @@ public class C206_CaseStudy {
 	public static void viewAllCurrency(ArrayList<Currency> currencyList) {
 
 		C206_CaseStudy.setHeader("Currency LIST");
-		String output = String.format("%-10s %-30s %-10.2f %-10.2f\n", "ISO", "NAME",
+		String output = String.format("%-10s %-30s %-10s %-10s\n", "ISO", "NAME",
 				 "BUY RATE", "SELL RATE");
 		 output += retrieveAllCurrency(currencyList);
 		System.out.println(output);
@@ -99,7 +99,7 @@ public class C206_CaseStudy {
 		String output = "";
 
 		for (int i = 0; i < currencyList.size(); i++) {
-			output += String.format("%-10s %-30s %-10s %-10s\\n", currencyList.get(i).getISO(), currencyList.get(i).getName(), currencyList.get(i).getbRate(),  currencyList.get(i).getsRate() );
+			output += String.format("%-10d %-30s %-10.2f %-10.2f\n", currencyList.get(i).getISO(), currencyList.get(i).getName(), currencyList.get(i).getbRate(),  currencyList.get(i).getsRate() );
 		}
 		return output;
 	}
